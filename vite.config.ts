f96818c91369e8ccae162ca1199a53847cc9b71e
Knowell Cedrich Riego <cedrich@Knowells-Laptop.local>
2025-01-28 09:39:37 +0800

    Sample

---
+++ vite.config.ts
@@ -0,0 +1,37 @@
+import { VitePWA } from 'vite-plugin-pwa';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react(), VitePWA({
+    strategies: 'injectManifest',
+    srcDir: 'src',
+    filename: 'sw.ts',
+    registerType: 'prompt',
+    injectRegister: false,
+
+    pwaAssets: {
+      disabled: false,
+      config: true,
+    },
+
+    manifest: {
+      name: 'vite-project',
+      short_name: 'vite-project',
+      description: 'vite-project',
+      theme_color: '#ffffff',
+    },
+
+    injectManifest: {
+      globPatterns: ['**/*.{js,css,html,svg,png,ico}'],
+    },
+
+    devOptions: {
+      enabled: true,
+      navigateFallback: 'index.html',
+      suppressWarnings: true,
+      type: 'module',
+    },
+  })],
+})
